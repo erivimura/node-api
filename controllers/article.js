5d6b3c3ec9080cf0aa6a4837b2faa9032ef02443
Erika Munini <erivimura@gmail.com>
2021-05-03 23:43:10 -0400

    Create endpoint Update

--- controllers/article.js
+++ controllers/article.js
@@ -116,6 +116,56 @@ var controller = {
                 article
             });
         });        
+    },
+
+    update: (req, res) => {
+        //Get id from url
+        var articleId = req.params.id;
+          
+        //Get data from body
+        var params = req.body;
+
+        //Validate data
+        try {
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+        } catch(err) {
+            return res.status(500).send({
+                status: 'error',
+                mensaje: 'Data missing!!!'
+            });
+        }
+
+        if (validate_title && validate_content) {
+            //Find and update (the new: true  returns the updated object)
+            Article.findOneAndUpdate({_id: articleId}, params, {new: true}, (err, articleUpdated) => {
+                if (err) {
+                    return res.status(500).send({
+                        status: 'error',
+                        mensaje: 'Error on update article'
+                    });
+                }
+    
+                if (!articleUpdated) {
+                    return res.status(404).send({
+                        status: 'error',
+                        mensaje: 'No updated article'
+                    });
+                }
+
+                //Respuesta
+                return res.status(200).send({
+                    status: 'success',
+                    article: articleUpdated
+                });
+            });
+            
+        } else {
+            return res.status(400).send({
+                status: 'error',
+                mensaje: 'Wrong data!!!'
+            });
+        }
     }
 }
 
--- routes/article.js
+++ routes/article.js
@@ -8,5 +8,6 @@ var router = express.Router();
 router.post('/save', ArticleController.save);
 router.get('/articles/:last?', ArticleController.getArticles); //The '?' indicates an optional value
 router.get('/article/:id', ArticleController.getArticle);
+router.put('/article/:id', ArticleController.update);
 
 module.exports = router;
