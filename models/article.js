40dac0b63fc62f122e0343d28ef5754db113c4da
Erika Munini <erivimura@gmail.com>
2021-05-03 23:14:31 -0400

    Create Model Article

---
+++ models/article.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+var ArticleSchema = schema({
+    title: String,
+    content: String,
+    date: { 
+            type: Date, 
+            default: Date.now 
+        },
+    image: String
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
+//articles --> save documents of this type and structure into collection
